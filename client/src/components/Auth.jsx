ddd09de25d36ff06bcb42f5a85007f1a64631914
Brilliant Kwakye <brilliantkwakye@Brilliants-MacBook-Pro.local>
2022-06-13 08:20:48 +0000

    Auth added

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signinImage from '../assets/signup.jpg';
+
+
+const Auth = () => {
+    const [isSignup, setIsSignup] = useState(true);
+    
+  return (
+    <div lassName="auth__form-container">
+        <div className="auth__form-container_fields">
+            <div className="auth__form-container_fields-content">
+            <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Auth;
