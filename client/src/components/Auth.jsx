a23fa9a027f803dc85c31d17734cadd31c01b7fa
Brilliant Kwakye <brilliantkwakye@Brilliants-MacBook-Pro.local>
2022-06-13 08:30:23 +0000

    Auth updated

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -7,12 +7,29 @@ import signinImage from '../assets/signup.jpg';
 
 const Auth = () => {
     const [isSignup, setIsSignup] = useState(true);
-    
+
+    const handleChange = () => {}
+
   return (
     <div lassName="auth__form-container">
         <div className="auth__form-container_fields">
             <div className="auth__form-container_fields-content">
             <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+            <form onSubmit={() => {}}>
+                {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input 
+                                    name="fullName" 
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                )}
+                
+            </form>
             </div>
         </div>
 
