8bb67b64daf1cddf4cb2d23c11dc5a21086bd290
Brilliant Kwakye <brilliantkwakye@Brilliants-MacBook-Pro.local>
2022-06-13 07:38:48 +0000

    some update

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainer } from './components';
+
+import './App.css';
+
+const apiKey = 'x7t2k6sud4bk';
+
+const client = StreamChat.getInstance(apiKey);
+
+function App() {
+  return (
+    <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer 
+            
+            
+            />
+
+            <ChannelContainer 
+            
+            />
+        </Chat>
+    </div>
+  )
+}
+
+export default App
