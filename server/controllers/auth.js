c4287f04a98ba6d820b2df0a8877a31dcfcb74ab
Brilliant Kwakye <brilliantkwakye@Brilliants-MacBook-Pro.local>
2022-06-14 10:32:07 +0000

    controller updated

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,13 +8,15 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
-const login = (req, res) => {
+const signup = async (req, res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
 
         const userId = crypto.randomBytes(16).toString('hex');
 
         const serverClient = connect(api_key, api_secret, app_id);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
 
     } catch (error) {
         console.log(error);
@@ -23,7 +25,7 @@ const login = (req, res) => {
     }
 };
 
-const signup = (req, res) => {
+const login = (req, res) => {
     try {
         
     } catch (error) {
