543ea311ccabe3d860841faf3672e6c580f82612
Brilliant Kwakye <brilliantkwakye@Brilliants-MacBook-Pro.local>
2022-06-14 11:21:25 +0000

    controller updated

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto');
 
+require('dotenv').config();
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
@@ -21,21 +22,37 @@ const signup = async (req, res) => {
         const token = serverClient.createUserToken(userId);
 
         res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
-
     } catch (error) {
         console.log(error);
 
-        res.status(500).json({ message: error});
+        res.status(500).json({ message: error });
     }
 };
 
 const login = async (req, res) => {
     try {
+        const { username, password } = req.body;
         
-    } catch (error) {
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: username });
+
+        if(!users.length) return res.status(400).json({ message: 'User not found' });
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success) {
+            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+        } else {
+            res.status(500).json({ message: 'Incorrect password' });
+        }
+    } catch (error) {ads
         console.log(error);
 
-        res.status(500).json({ message: error});
+        res.status(500).json({ message: error });
     }
 };
 
